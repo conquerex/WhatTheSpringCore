746e48992ad574ecc6449589e6238e8d9f42ce8b
Conquerex <conquerex@gmail.com>
2021-05-11 12:16:13 +0900

    AppConfig 리팩터링 ~ 좋은 객체 지향 설계의 5가지 원칙의 적용

--- src/main/java/what/the/springcore/AppConfig.java
+++ src/main/java/what/the/springcore/AppConfig.java
@@ -1,6 +1,9 @@
 package what.the.springcore;
 
+import what.the.springcore.discount.DiscountPolicy;
 import what.the.springcore.discount.FixDiscountPolicy;
+import what.the.springcore.discount.RateDiscountPolicy;
+import what.the.springcore.member.MemberRepository;
 import what.the.springcore.member.MemberService;
 import what.the.springcore.member.MemberServiceImpl;
 import what.the.springcore.member.MemoryMemberRepository;
@@ -9,11 +12,20 @@ import what.the.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderServiceImpl orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
