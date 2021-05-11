0f85c4212d9a279720e0f9e7dee1c0013b073e15
Conquerex <conquerex@gmail.com>
2021-05-11 17:35:58 +0900

    다양한 설정 형식 지원 ~ 스프링 빈 설정 메타 정보

---
+++ src/test/java/what/the/springcore/BeanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,30 @@
+package what.the.springcore.BeanDefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import what.the.springcore.AppConfig;
+
+public class BeanDefinitionTest {
+
+// beanDefinition = Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=appConfig; factoryMethodName=memberService; initMethodName=null; destroyMethodName=(inferred); defined in what.the.springcore.AppConfig
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+// beanDefinition = Generic bean: class [what.the.springcore.member.MemberServiceImpl]; scope=; abstract=false; lazyInit=false; autowireMode=0; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=null; factoryMethodName=null; initMethodName=null; destroyMethodName=null; defined in class path resource [appConfig.xml]
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinition name = " + beanDefinitionName);
+                System.out.println("beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
